d000a5d2a4585fa571afab7442e338ebde921a0d
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-09-16 11:57:44 +0530

    db connection established with clusters

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const { phloi_db_connection } = require("./config/config")
 
 app.get('/', (req, res) => {
     res.send("I am online");
 });
+
+phloi_db_connection()
 
 const PORT = 8000;
 
