199b7356059618f290881e367c318376addcc43f
akshita-tomar <akshita.tomar@ultivic.com>
2024-09-17 18:18:16 +0530

    'remove_error'

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,46 @@
-let express = require('express')
-let router = express.Router()
-const userController = require("../controllers/userController")
-const { get_user_detail_validator, user_registration_steps_validator } = require("../middlewares/validationMiddleware")
-
-// user routes
-router.put("/user_registration_steps", user_registration_steps_validator, userController.user_registration_steps)
-router.get("/get_user_details/:id?", get_user_detail_validator, userController.get_user_details)
+const mongoose = require('mongoose');
 
 
+const UserCharactersticsSchema = new mongoose.Schema({
+    communication_style_id: { type: mongoose.Schema.Types.ObjectId, ref: 'communication_style' },
+    love_receive_id: { type: mongoose.Schema.Types.ObjectId, ref: 'love_receive' },
+    drink_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'drink_frequency' },
+    smoke_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'smoke_frequency' },
+    workout_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'workout_frequency' },
+    interests_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Interests' }],
+});
 
-//user 
-router.post("/login",userController.login)
-router.post("/social_login",userController.social_login)
-router.post("/verify_otp",userController.verify_otp)
+
+const UserPreferencesSchema = new mongoose.Schema({
+    sexual_orientation_preference_id: { type: mongoose.Schema.Types.ObjectId, ref: 'sexual_orientation' },
+    relationship_type_preference_id: { type: mongoose.Schema.Types.ObjectId, ref: 'relationship_preference' },
+    distance_preference: { type: Number, default: 0 }
+});
 
 
-module.exports = router
+const userSchema = new mongoose.Schema({
+    username: { type: String, default: null },
+    mobile_number: { type: Number, default: null },
+    email: { type: String, default: null },
+    otp:{type:String,default:null},
+    otp_sent_at:{type:Date,default:null},
+    dob: { type: Date, default: null },
+    gender: { type: String, enum: ["women", "men", "other"] },
+    study: { type: String, default: null },
+    intrested_to_see: { type: String, enum: ['men', "women", "everyone"], default: 'everyone' },
+    completed_steps:{type:Array,default:[]},
+    current_step:{type:Number,default:0},
+    socialLogin: [
+        {
+            providerName: { type: String, enum: ["google", "apple", "facebook"] },
+            providerId: { type: String, default: null }
+        },
+    ],
+    characteristics: UserCharactersticsSchema,
+    preferences: UserPreferencesSchema,
+
+}, { timestamps: true });
+
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,18 @@
 let express = require('express')
 let router = express.Router()
+const userController = require("../controllers/userController")
+const { get_user_detail_validator, user_registration_steps_validator } = require("../middlewares/validationMiddleware")
+
+// user routes
+router.put("/user_registration_steps", user_registration_steps_validator, userController.user_registration_steps)
+router.get("/get_user_details/:id?", get_user_detail_validator, userController.get_user_details)
 
 
 
+//user 
+router.post("/login",userController.login)
+router.post("/social_login",userController.social_login)
+router.post("/verify_otp",userController.verify_otp)
 
 
 module.exports = router
