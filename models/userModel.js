6abb871fd08bf669d556055981e0ef5d9bd09693
akshita-tomar <akshita.tomar@ultivic.com>
2024-09-17 12:21:42 +0530

    'added_distance_preference'

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,44 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-let userSchema = new mongoose.Schema({
-    username:{type:String,default:null},
-    mobile_number :{type:Number,default:null},
-    email:{type:String,default:null},
-    dob:{type:Date,default:null},
-    gender:{type:String,enum:["women","men","other"]},
-    schooling:{type:String,default:null},
-    intrested_to_see:{type:String,enum:['men',"women","everyone"],default:'everyone'},
-},{timestamps:true}) 
 
-let userModel = mongoose.model('User',userSchema)
-module.exports = userModel
+const UserCharactersticsSchema = new mongoose.Schema({
+    communication_style_id: { type: mongoose.Schema.Types.ObjectId, ref: 'communication_style' },
+    love_receive_id: { type: mongoose.Schema.Types.ObjectId, ref: 'love_receive' },
+    drink_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'drink_frequency' },
+    smoke_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'smoke_frequency' },
+    workout_frequency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'workout_frequency' },
+    interests_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Interests' }],
+});
+
+
+const UserPreferencesSchema = new mongoose.Schema({
+    sexual_orientation_preferece_id: { type: mongoose.Schema.Types.ObjectId, ref: 'sexual_orientation' },
+    relationship_type_preference_id: { type: mongoose.Schema.Types.ObjectId, ref: 'relationship_preference' },
+    distance_preference: { type: Number ,default:0}
+});
+
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, default: null },
+    mobile_number: { type: Number, default: null },
+    email: { type: String, default: null },
+    dob: { type: Date, default: null },
+    gender: { type: String, enum: ["women", "men", "other"] },
+    schooling: { type: String, default: null },
+    intrested_to_see: { type: String, enum: ['men', "women", "everyone"], default: 'everyone' },
+    completed_steps:{type:Array,default:null},
+    current_step:{type:Number,default:0},
+    socialLogin: [
+        {
+            providerName: { type: String, enum: ["google", "apple", "facebook"] },
+            providerId: { type: String, default: null }
+        },
+    ],
+    characteristics: UserCharactersticsSchema,
+    preferences: UserPreferencesSchema,
+
+}, { timestamps: true });
+
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
