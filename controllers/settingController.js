f9a372b2e26d2534577fd2c12816d441c83669ca
akshita-tomar <akshita.tomar@ultivic.com>
2024-11-13 17:11:33 +0530

    'add_title_in_setting_pages'

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -32,24 +32,52 @@ exports.get_settings_info = async (req, res) => {
 
 exports.get_setting_page_details = async (req, res) => {
     try {
-        let pageId = req.query.pageId
+        let pageId = req.query.pageId;
 
-        if (!pageId) { return res.status(400).json(errorResponse(messages.generalError.somethingWentWrong, 'Please provide page Id')) }
-
-        const settingDocument = await settingModel.findOne({ 'pages._id': pageId }, { 'pages.$': 1 });
-
-        if (settingDocument && settingDocument.pages.length > 0) {
-            const page = settingDocument.pages[0]; 
-    
-            return res.status(200).json(successResponse('Data retrieved successfully',page.content))
-        } else {
-            console.log('Page not found.');
+     
+        if (!pageId) {
+            return res.status(400).json(errorResponse(messages.generalError.somethingWentWrong, 'Please provide page Id'));
         }
 
+      
+        const settingDocument = await settingModel.findOne({ 'pages._id': pageId }, { 'pages.$': 1 });
+
+       
+        if (!settingDocument) {
+            return res.status(404).json(errorResponse(messages.generalError.notFound, 'Page not found'));
+        }
+
+        
+        if (settingDocument.pages.length === 0) {
+            return res.status(404).json(errorResponse(messages.generalError.notFound, 'Page not found in the setting document'));
+        }
+
+       
+        const page = settingDocument.pages[0];
+        
+
+        
+        if (!page.content) {
+            return res.status(404).json(errorResponse(messages.generalError.notFound, 'Page content not found'));
+        }
+         
+    
+        return res.status(200).json({type:'success',message:'Data retrieved successfully',title:page.title,data:page.content})
+       
+
     } catch (error) {
-        console.log("ERROR::", error)
-        return res.status(500).json(errorResponse(messages.generalError.somethingWentWrong, error.message))
+       
+        console.log("ERROR::", error);
+
+       
+        if (error.name === 'ValidationError') {
+            return res.status(400).json(errorResponse(messages.generalError.validationError, error.message));
+        }
+
+       
+        return res.status(500).json(errorResponse(messages.generalError.somethingWentWrong, error.message));
     }
-}
+};
+
 
 
