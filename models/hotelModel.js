5f7b455c35d6039dcf7e9f372eda2ff5dbafab77
akshita-tomar <akshita.tomar@ultivic.com>
2024-12-23 21:36:23 +0530

    new changes aved

--- models/hotelModel.js
+++ models/hotelModel.js
@@ -23,7 +23,7 @@ const hotelSchema = new mongoose.Schema({
     inPersonVisitAvailability: { type: String },
     images: [{ type: String }],
     onboardingCompleted: { type: Boolean, default: false },
-    adminVerified: { type: Boolean, default: null },
+    adminVerified: { type: Boolean,default:false },
     paymentStatus: { type: String, enum: ["pending", "completed", "failed"], default: "pending" },
     subscriptionEndDate: { type: Date },
     customerServiceNumber: { type: String },
